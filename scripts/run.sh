b9ed2448e0322f7ac5e28b97cb38fb439bb250d4
martinraynov <mraynov@gmail.com>
2024-11-30 18:12:24 +0100

    Update run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,11 @@
 # Requirements : you must have make installed on your system
 # Usage: Only as installed executable ! Use APP_NAME <command> (when installed as executable)
 
-APP_PATH=$(dirname $(find -L /usr/local/bin/traefik -exec readlink -f {} +))
+# Get the name of the current app
+APP_NAME=$(basename "$0")
+# Get the origin path of the current app
+APP_PATH=$(dirname $(find -L /usr/local/bin/${APP_NAME} -exec readlink -f {} +))
+
 if [ -n "$1" ]; then
     (cd ${APP_PATH} && cd .. && ${pwd} && make $1)
 else
